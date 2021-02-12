ef9ed0712a2798850573c49fce14da2e9c411396
Kendako <kendako361@gmail.com>
2021-02-12 16:36:18 -0300

    skills prontas

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
-	
 	<link rel="stylesheet" type="text/css" href="/style/estilo.css">
 
 	
@@ -16,18 +15,16 @@
 
 </head>
 <body>
-
-	<!-- Nav Bar -->
+	<!-- Navbar -->
 	<nav class="navbar">
 		<div class="inner-width">
-			<a href="#" class="logo"></a>
-			<button class="menu-toggler active">
+			<a href="/" class="logo"></a>
+			<button class="menu-toggler">
 			<span></span>
 			<span></span>
 			<span></span>
 			</button>
-
-			<div class="navbar-menu active">
+			<div class="navbar-menu">
 				<a href="#">Home</a>
 				<a href="#">Sobre</a>
 				<a href="#">Skills</a>
@@ -59,8 +56,61 @@
 		</div>
 	</section>
 
-	<p>What is Lorem Ipsum?
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	<!-- Home -->
+	<section id="sobre">
+		<div class="inner-width">
+			<h1 class="section-title">Sobre Mim</h1>
+			<div class="about-content">
+				<img src="images/pic.png" alt="" class="about-pic">
+			
+				<div class="about-text">
+				<h2>Olá Eu sou Paulo</h2>
+				<h3>
+				<span>Desenvolvedor</span>
+				<span>Freelancer</span>
+				<span>Streammer</span>
+				</h3>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, fugit quasi dolor ut nemo laudantium. Fugit voluptatibus eaque minus iure natus consequatur? Enim debitis magni voluptate voluptatem voluptates culpa delectus, aliquid fuga harum nam modi eligendi quod, exercitationem, quo blanditiis dolorem beatae! Ut, ipsum perspiciatis sit delectus cupiditate nobis unde praesentium explicabo dolorem. Ipsa, quo tempore deserunt suscipit aspernatur sequi consectetur cupiditate ea laborum eos voluptatibus. Repellendus, atque magnam? Magni?</p>
+				</div>
+			</div>
+
+			<div class="skills">
+				<div class="skill">
+						<div class="skill-info">
+						<span>HTML</span>
+						<span>50%</span>
+						</div>
+					<div class="skill-bar html"></div>
+					</div>
+
+				<div class="skill">
+					<div class="skill-info">
+					<span>CSS</span>
+					<span>60%</span>
+					</div>
+					<div class="skill-bar css"></div>
+					</div>
+
+				<div class="skill">
+						<div class="skill-info">
+						<span>PHP</span>
+						<span>35%</span>
+						</div>
+					<div class="skill-bar php"></div>
+					</div>
+
+					<div class="skill">
+						<div class="skill-info">
+						<span>Javascript</span>
+						<span>10%</span>
+						</div>
+					<div class="skill-bar js"></div>
+					</div>
+				
+
+			</div>
+		</div>
+	</section>
 
 </body>
 
