b3edf946ac4bbd7f44ab24d49cef18c0214121ba
Kendako <kendako361@gmail.com>
2021-02-16 04:13:11 -0300

    Base Concluida necessários ajustes e adição de conteúdo

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css">
 	<link rel="stylesheet" type="text/css" href="/style/estilo.css">
 
 	
@@ -25,10 +26,10 @@
 			<span></span>
 			</button>
 			<div class="navbar-menu">
-				<a href="#">Home</a>
-				<a href="#sobre">Sobre & Skills</a>
-				<a href="#">Works</a>
-				<a href="#">Contato</a>
+				<a href="#home">Home</a>
+				<a href="#sobre">Sobre</a>
+				<a href="#skills">Skills</a>
+				<a href="#contato">Contato</a>
 			</div>
 		</div>
 	</nav>
@@ -41,15 +42,15 @@
 				<h1>Olá Eu sou </h1>
 				<div class="sm">
 
-					<a href="#" class="fab fa-facebook-f"></a>
-					<a href="#" class="fab fa-instagram"></a>
-					<a href="#" class="fab fa-linkedin-in"></a>
-					<a href="#" class="fab fa-github"></a>
+					<a href="https://www.facebook.com/PauloKendako" class="fab fa-facebook-f" target="_blank"></a>
+					<a href="https://www.instagram.com/paulo_matos0/" class="fab fa-instagram" target="_blank"></a>
+					<a href="https://www.linkedin.com/in/antonio-paulo-matos-293715205/" class="fab fa-linkedin-in" target="_blank"></a>
+					<a href="https://github.com/paulokmatos" class="fab fa-github" target="_blank"></a>
 				</div>
 
 				<div class="buttons">
-					<a href="#">Contact Me</a>
-					<a href="#">Curriculum</a>
+					<a href="#contato">Contact Me</a>
+					<a href="https://drive.google.com/file/d/1WnIo0gojP6aeCjvJG9hQ1Wzwcgr3ozx5/view?usp=sharing" target="_blank">Curriculum</a>
 				</div>
 			</div>
 		</div>
@@ -74,80 +75,50 @@
 			</div>
 
 
-			<div id="skills">
-			<div class="skills">
-				<div class="skill">
-						<div class="skill-info">
-						<span>HTML</span>
-						<span>50%</span>
-						</div>
-					<div class="skill-bar html"></div>
-					</div>
-
-				<div class="skill">
-					<div class="skill-info">
-					<span>CSS</span>
-					<span>60%</span>
-					</div>
-					<div class="skill-bar css"></div>
-					</div>
-
-				<div class="skill">
-						<div class="skill-info">
-						<span>PHP</span>
-						<span>35%</span>
-						</div>
-					<div class="skill-bar php"></div>
-					</div>
-
-					<div class="skill">
-						<div class="skill-info">
-						<span>Javascript</span>
-						<span>10%</span>
-						</div>
-					<div class="skill-bar js"></div>
-					</div>
-				
-
-			</div>
-			</div>
+			
 		</div>
 	</section>
 
 	<!-- Serviços -->
 
-	<section id="services" class="dark" >
+	<section id="skills" class="dark" >
 		<div class="inner-width">
-			<h1 class="section-title">Serviços</h1>
-			<div class="services">
+			<h1 class="section-title">Minhas Skills</h1>
+			<div class="skills">
 
-			<div class="service">
-				<i class="icon fas fa-file-code"></i>
-				<h4>Developer</h4>
-				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, explicabo.</p>
-			</div>
-
-			<div class="service">
+			<div class="skill">
 				<i class="icon fab fa-html5"></i>
 				<h4>HTML 5</h4>
 				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, explicabo.</p>
 			</div>
 
-			<div class="service">
+			<div class="skill">
 				<i class="icon fab fa-css3-alt"></i>
 				<h4>CSS 3</h4>
 				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, explicabo.</p>
 			</div>
 
-			<div class="service">
+			<div class="skill">
+				<i class="icon fab fa-js"></i>
+				<h4>JavaScript</h4>
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, explicabo.</p>
+			</div>
+
+			<div class="skill">
 				<i class="icon fas fa-database"></i>
 				<h4>My SQL</h4>
 				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, explicabo.</p>
 			</div>
 
-			<div class="service">
+			<div class="skill">
 				<i class="icon fab fa-php"></i>
 				<h4>PHP</h4>
+				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, explicabo.</p>
+			</div>
+
+			<div class="skill">
+				<i class="icon fab fa-sass"></i>
+				<h4>Scss</h4>
 				<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio, explicabo.</p>
 			</div>
 
@@ -156,8 +127,126 @@
 		</div>
 	</section>
 
+	<!-- Educação e Experiencia -->
+
+	<section id="education">
+		<div class="inner-width">
+			<h1 class="section-title">Educação e Experiência</h1>
+			<div class="time-line">
+
+				<div class="bloco">
+					<h4>2019-2019</h4>
+					<h3>Estagiário de Programação</h3>
+					<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis numquam ipsum nihil facilis tempora laboriosam possimus fugiat minus quo repellat?</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Portfolio e Projetos -->
+
+	<section id="portfolio" class="dark">
+		<div class="inner-width">
+			<h1 class="section-title">Projetos</h1>
+			<div class="projetos">
+
+				<a href="https://paulokmatos.github.io/calculadora-css/" class="projeto" target="_blank">
+				<img src="images/works/calc.jpg" alt="">
+				<div class="info">
+					<h3>Calculadora Css</h3>
+					<div class="cat">HTML, CSS, SASS, JavaScript</div>
+				</div>
+			</a>
+
+			<a href="images/works/2.jpg" class="projeto">
+				<img src="images/works/2.jpg" alt="">
+				<div class="info">
+					<h3>Work Title</h3>
+					<div class="cat">Music, Headset</div>
+				</div>
+			</a>
+
+			<a href="images/works/3.jpg" class="projeto">
+				<img src="images/works/3.jpg" alt="">
+				<div class="info">
+					<h3>Work Title</h3>
+					<div class="cat">Music, Headset</div>
+				</div>
+			</a>
+
+			<a href="images/works/4.jpg" class="projeto">
+				<img src="images/works/4.jpg" alt="">
+				<div class="info">
+					<h3>Work Title</h3>
+					<div class="cat">Music, Headset</div>
+				</div>
+			</a>
+
+			</div>
+		</div>
+	</section>
+
+	<!-- Contato -->
+	<section id="contato">
+		<div class="inner-width">
+			<h1 class="section-title">Contato</h1>
+			<div class="contact-info">
+
+				<div class="item">
+					<i class="fas fa-mobile-alt"></i>
+					+55 (74) 99912-0547
+				</div>
+				
+
+			
+				<div class="item">
+					<i class="fas fa-envelope"></i>
+					Kendako361@gmail.com
+				</div>
+			
+
+				<div class="item">
+					<i class="fas fa-map-marker-alt"></i>
+					Belo Horizonte-MG Brazil
+				</div>
+
+
+			</div>
+
+			<form action="" class="contact-form">
+				<input type="text" class="nameZone" placeholder="Seu Nome Completo">
+				<input type="email" class="emailZone" placeholder="Seu Email">
+				<input type="text" class="subjectZone" placeholder="Assunto">
+				<textarea class="messageZone" placeholder="Sua Menssagem"></textarea>
+				<input type="submit" value="Enviar" class="btn">
+			</form>
+		</div>
+	</section>
+
+	<!-- Footer -->
+	<footer>
+		<div class="inner-width">
+			<div class="copyright">
+				&copy; 2021 | Created and Designed by <a href="#">Antonio Paulo Matos</a>
+			</div>
+
+			<div class="sm">
+				<a href="https://www.facebook.com/PauloKendako" class="fab fa-facebook-f" target="_blank"></a>
+				<a href="https://www.instagram.com/paulo_matos0/" class="fab fa-instagram" target="_blank"></a>
+				<a href="https://www.linkedin.com/in/antonio-paulo-matos-293715205/" class="fab fa-linkedin-in" target="_blank"></a>
+				<a href="https://github.com/paulokmatos" class="fab fa-github" target="_blank"></a>
+				
+			</div>
+
+		</div>
+	</footer>
+
+	<!-- Go Top BTN -->
+	<button class="goTop fas fa-arrow-up"></button>
+
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="/script/script.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js"></script>
 
 </html>
